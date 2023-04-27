474076f70421ad62a431ddfcebe6f5d34ea136ca
wyj <tset@test.com>
2023-04-27 10:38:05 +0800

    commit: Modify main.py comments.

--- src/main.py
+++ src/main.py
@@ -4,22 +4,22 @@ from Facial_landmark import classification_model as ClF
 import cv2
 import numpy as np
 
-# def regression():
-#     rgs = RGS.create_regressionmodel()
-#     img_y = rgs.SVR_mod(imgpath='./src/example/f_001.jpg')
-#     print('BMI value',img_y)
+def regression():# SVR for example
+    rgs = RGS.create_regressionmodel()
+    img_y = rgs.SVR_mod(imgpath='./src/example/f_001.jpg')
+    print('BMI value',img_y)
 
-# def classification():
-#     clf = ClF.create_classificationmodel()
-#     img_y = clf.RFC_mod(imgpath='./src/example/f_001.jpg')
-#     print('BMI type',img_y)
+def classification():# Random Forests for example
+    clf = ClF.create_classificationmodel()
+    img_y = clf.RFC_mod(imgpath='./src/example/f_001.jpg')
+    print('BMI type',img_y)
 
-def read_SVR_modle():
+def read_SVR_modle():# Example of saved model
     rgs = RGS.create_regressionmodel()
     img_y = rgs.SVR_mod(imgpath='./src/example/f_001.jpg',modpath='./src/data/SVR_lm_BMI.pkl')
     print('BMI value',img_y)
 
 if __name__ == '__main__':
-    # regression()
-    # classification()
+    regression()
+    classification()
     read_SVR_modle()
