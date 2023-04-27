bd355433a75c37b06c27060aa3bf61a0184d0a85
wyj <tset@test.com>
2023-04-27 09:08:41 +0800

    commit: Update readme.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Facial-Landmark-based-BMI-Analysis-
+# Facial-Landmark-based-BMI-Analysis
+## Datasets
+-The dataset we used comes from two previous studies, namely the ![VIP](https://ieeexplore.ieee.org/abstract/document/8546159) dataset and the ![VB](https://ieeexplore.ieee.org/abstract/document/8666768) dataset.
--- src/main.py
+++ src/main.py
@@ -6,13 +6,13 @@ import numpy as np
 
 def regression():
     rgs = RGS.create_regressionmodel()
-    img_y = rgs.SVR_mod(imgpath='./paper_data/f_001.jpg')
-    print(img_y)
+    img_y = rgs.SVR_mod(imgpath='./src/example/f_001.jpg')
+    print('BMI value',img_y)
 
 def classification():
     clf = ClF.create_classificationmodel()
-    img_y = clf.RFC_mod(imgpath='./paper_data/f_001.jpg')
-    print(img_y)
+    img_y = clf.RFC_mod(imgpath='./src/example/f_001.jpg')
+    print('BMI type',img_y)
 
 if __name__ == '__main__':
     regression()
